870ba8f3c988680a9d33f7df70e5f848455bc586
Marton Gábor <mgr477@gmail.com>
2017-04-29 15:11:25 +0200

    Solutions for tail with tailrec

--- src/main/scala/tail_trampoline/tail/TailTailRec.scala
+++ src/main/scala/tail_trampoline/tail/TailTailRec.scala
@@ -9,5 +9,40 @@ import scalaz.Maybe.{Empty, Just}
 /**
   */
 object TailTailRec extends Tail {
-  override def lastNLines(n: Int, is: InputStream): Seq[String] = ???
+  override def lastNLines(n: Int, is: InputStream): Seq[String] = {
+    @tailrec
+    def helper(acc: Seq[String]): Seq[String] = {
+      val ch = is.read()
+      if (n == 0) Seq[String]()
+      else
+      if (ch < 0)
+        acc match {
+          case Nil => Seq("")
+          case head :: Nil => if (head == "\n") Seq("", "").takeRight(n) else head :: Nil
+          case head :: tail => if (tail.last == "\n") (head :: tail.init ++ Seq("")).takeRight(n) else head :: tail
+        }
+      else {
+        val nextAcc: Seq[String] =
+          if (ch == '\n')
+            acc match {
+              case Nil => Seq("\n")
+              case "\n" :: Nil => Seq("", "\n").takeRight(n)
+              case head :: Nil => Seq(head, "\n").takeRight(n)
+              case head :: tail =>
+                (if (tail.last == "\n") head :: tail.init ++ Seq("", "\n") else head :: tail ++ Seq("\n")).takeRight(n)
+            }
+          else
+            acc match {
+              case Nil => Seq(ch.toChar.toString)
+              case "\n" :: Nil => Seq(ch.toChar.toString)
+              case head :: Nil => Seq(head + ch.toChar.toString)
+              case head :: tail => if (tail.last == "\n") head :: tail.init ++ Seq(ch.toChar.toString)
+                                   else head :: tail.init ++ Seq(tail.last + ch.toChar.toString)
+            }
+
+        helper(nextAcc)
+      }
+    }
+    helper(Seq[String]())
+  }
 }
