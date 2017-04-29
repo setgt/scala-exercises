4d5f4e9e119142286532d9762380afb8293c7e2a
Marton Gábor <mgr477@gmail.com>
2017-04-29 21:02:30 +0200

    Remove takeRight calls, add trampoline solution

--- src/main/scala/tail_trampoline/tail/TailTailRec.scala
+++ src/main/scala/tail_trampoline/tail/TailTailRec.scala
@@ -11,38 +11,48 @@ import scalaz.Maybe.{Empty, Just}
 object TailTailRec extends Tail {
   override def lastNLines(n: Int, is: InputStream): Seq[String] = {
     @tailrec
-    def helper(acc: Seq[String]): Seq[String] = {
+    def helper(acc: (Int, Seq[String])): (Int, Seq[String]) = {
       val ch = is.read()
-      if (n == 0) Seq[String]()
+      if (n == 0) (0, Seq[String]())
       else
       if (ch < 0)
         acc match {
-          case Nil => Seq("")
-          case head :: Nil => if (head == "\n") Seq("", "").takeRight(n) else head :: Nil
-          case head :: tail => if (tail.last == "\n") (head :: tail.init ++ Seq("")).takeRight(n) else head :: tail
+          case (prevLen, Nil) => (1, Seq(""))
+          case (prevLen, head :: Nil) =>
+            if (head == "\n")
+              if (prevLen < n) (2,(Seq("", ""))) else (1, Seq(""))
+            else (1, head :: Nil)
+          case (prevLen, head :: tail) => if (tail.last == "\n") (prevLen, (head :: tail.init ++ Seq("")))
+                                          else (prevLen, head :: tail)
         }
       else {
-        val nextAcc: Seq[String] =
+        val nextAcc: (Int, Seq[String]) =
           if (ch == '\n')
             acc match {
-              case Nil => Seq("\n")
-              case "\n" :: Nil => Seq("", "\n").takeRight(n)
-              case head :: Nil => Seq(head, "\n").takeRight(n)
-              case head :: tail =>
-                (if (tail.last == "\n") head :: tail.init ++ Seq("", "\n") else head :: tail ++ Seq("\n")).takeRight(n)
+              case (_,Nil) => (1, Seq("\n"))
+              case (prevLen, "\n" :: Nil) => if (prevLen < n) (prevLen + 1 , Seq("", "\n")) else (n, Seq("\n"))
+              case (prevLen, head :: Nil) => if (prevLen < n) (prevLen + 1, Seq(head, "\n")) else (n, Seq("\n"))
+              case (prevLen, head :: tail) =>
+                if (tail.last == "\n")
+                     if (prevLen < n) (prevLen + 1, head :: tail.init ++ Seq("", "\n")) else (n, tail.init ++ Seq("", "\n"))
+                else
+                     if (prevLen < n) (prevLen + 1, head :: tail ++ Seq("\n")) else (n, tail ++ Seq("\n"))
             }
           else
             acc match {
-              case Nil => Seq(ch.toChar.toString)
-              case "\n" :: Nil => Seq("", ch.toChar.toString).takeRight(n)
-              case head :: Nil => Seq(head + ch.toChar.toString)
-              case head :: tail => (if (tail.last == "\n") head :: tail.init ++ Seq(ch.toChar.toString)
-                                   else head :: tail.init ++ Seq(tail.last + ch.toChar.toString)).takeRight(n)
+              case (_, Nil) => (1, Seq(ch.toChar.toString))
+              case (_, "\n" :: Nil) => if (n > 1) (2, Seq("", ch.toChar.toString)) else (1, Seq(ch.toChar.toString))
+              case (_, head :: Nil) => (1, Seq(head + ch.toChar.toString))
+              case (prevLen, head :: tail) =>
+                if (tail.last == "\n")
+                  (prevLen, head :: tail.init ++ Seq(ch.toChar.toString))
+                else
+                  (prevLen, head :: tail.init ++ Seq(tail.last + ch.toChar.toString))
             }
 
         helper(nextAcc)
       }
     }
-    helper(Seq[String]())
+    helper(0, Seq[String]())._2
   }
 }
--- src/main/scala/tail_trampoline/tail/TailTrampoline.scala
+++ src/main/scala/tail_trampoline/tail/TailTrampoline.scala
@@ -3,10 +3,31 @@ package tail_trampoline.tail
 import java.io.InputStream
 
 import scalaz.Free.Trampoline
+import scalaz.Trampoline
 import scalaz.Trampoline._
 
 /**
   */
 object TailTrampoline extends Tail {
-  override def lastNLines(n: Int, is: InputStream): Seq[String] = ???
+  override def lastNLines(n: Int, is: InputStream): Seq[String] =  {
+    def helper(): Trampoline[Either[Seq[String], Seq[String]]] = {
+      val ch = is.read()
+      if (ch < 0) if (n == 0) Trampoline.done(Right(Vector.empty[String])) else Trampoline.done(Left(Vector("")))
+      else {
+        Trampoline.suspend(helper).flatMap {
+         sub => Trampoline.done(sub match
+          {
+            case result@Right(_) => result
+            case Left(seq) if ch == '\n' =>
+              if (seq.size == n) Right(seq) else Left("" +: seq)
+            case Left(current +: rest) => Left(s"${ch.toChar}$current" +: rest)
+          })
+        }
+      }
+    }
+    helper().run match {
+      case Left(seq) => seq
+      case Right(seq) => seq
+    }
+  }
 }
