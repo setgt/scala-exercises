1d79e8ebdbfa8d15d265bed311021c82beb432ec
Marton Gábor <mgr477@gmail.com>
2017-04-29 11:02:28 +0200

    Solutions for longest repeat from martong

--- src/main/scala/tail_trampoline/longest_repeat/LongestRepeatTailRec.scala
+++ src/main/scala/tail_trampoline/longest_repeat/LongestRepeatTailRec.scala
@@ -18,5 +18,31 @@ case object LongestRepeatTailRec extends LongestRepeat {
 
 //  override def firstLongestRepeat(observable: Observable[Char]): Result = ???
 
-  override def firstLongestRepeat(enumerator: IEnumerator): Result = ???
+  override def firstLongestRepeat(enumerator: IEnumerator): Result = {
+    val monoid = Result.resultMonoid
+    @tailrec
+    def helper(read: Int, bestPar: Result, currentPar: Result): (Result, Result) = {
+      if (!enumerator.moveNext) {
+        (bestPar, currentPar)
+      } else {
+        val forwardPar = {
+          // Call before next iteration as it moves to next!
+          val ch = enumerator.current
+          //val (best, current) = helper(read + 1)
+          currentPar.character match {
+            case None =>
+              (Result(read, Some(ch), 1), Result(read, Some(ch), 1))
+            case Some(c) if c == ch =>
+              val nextCurrent = Result(currentPar.firstPosition, Some(c), currentPar.length + 1)
+              (monoid.append(nextCurrent, bestPar), nextCurrent)
+            case _ =>
+              val nextCurrent = Result(read, Some(ch), 1)
+              (monoid.append(nextCurrent, bestPar), nextCurrent)
+          }
+        }
+        helper(read + 1, forwardPar._1, forwardPar._2)
+      }
+    }
+    helper(0, monoid.zero, monoid.zero)._1
+  }
 }
--- src/main/scala/tail_trampoline/longest_repeat/LongestRepeatTrampoline.scala
+++ src/main/scala/tail_trampoline/longest_repeat/LongestRepeatTrampoline.scala
@@ -3,17 +3,42 @@ package tail_trampoline.longest_repeat
 import rx.Observable
 
 import scala.util.control.TailCalls._
+import scalaz.Free.Trampoline
+import scalaz.Trampoline
 
 /**
   */
 case object LongestRepeatTrampoline extends LongestRepeat {
 
-  override def firstLongestRepeat(sequence: CharSequence): Result = ???
+  override def firstLongestRepeat(sequence: CharSequence): Result = firstLongestRepeat(Helpers.CharSequenceAsIEnumerator(sequence).enumerator)
 
   import Helpers._
   override def firstLongestRepeat(iterator: Iterator[Char]): Result = firstLongestRepeat(iterator.toEnumerator)
 
 //  override def firstLongestRepeat(observable: Observable[Char]): Result = ???
 
-  override def firstLongestRepeat(enumerator: IEnumerator): Result = ???
+  override def firstLongestRepeat(enumerator: IEnumerator): Result =  {
+    val monoid = Result.resultMonoid
+    def helper(read: Int): Trampoline[(Result, Result)] = {
+      if (!enumerator.moveNext) {
+        Trampoline.done((monoid.zero,monoid.zero))
+      } else {
+        // Call before next iteration as it moves to next!
+        val ch = enumerator.current
+        Trampoline.suspend(helper(read + 1)).flatMap { case (best: Result, current: Result) =>
+          Trampoline.done {
+            current.character match {
+              case None => (Result(read, Some(ch), 1), Result(read, Some(ch), 1))
+              case Some(c) if c == ch =>
+                val nextCurrent = Result(read, Some(c), current.length + 1)
+                (monoid.append(nextCurrent, best), nextCurrent)
+              case _ => val nextCurrent = Result(read, Some(ch), 1)
+                (monoid.append(nextCurrent, best), nextCurrent)
+            }
+          }
+        }
+      }
+    }
+    helper(0).run._1
+  }
 }
